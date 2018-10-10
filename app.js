bcc3880896b44fb4bac74593b8b4b5797d1478fa
rabee <rabee.dameer@gmail.com>
2018-10-10 12:09:20 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const Boom = require('boom');
+
+
+
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+
+app.use('/users', usersRouter);
+
+
+app.use((req, res, next)=> {    
+    const error = Boom.notFound('Sorry!! what you are looking for is not found');
+    next(error);
+});
+
+app.use((err, req, res, next)=> {
+    res.status(err.output.statusCode).json(err.output.payload);
+
+    next(err);
+});
+
+
+
+module.exports = app;
